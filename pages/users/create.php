379ceaee4f5417ae04bde14e0c96a5972f576f33
Jesse B. Crawford <jesse@jbcrawford.us>
2013-11-06 23:00:03 -0700

    merging

--- pages/users/create.php
+++ pages/users/create.php
@@ -3,22 +3,29 @@ require_once(dirname(__FILE__)."/../../resources/globals.php");
 require_once(dirname(__FILE__)."/../../resources/common_functions.php");
 
 function draw_create_user_form() {
-	$a_retval = array();
-	$a_retval[] = '<form id="create_user_form">';
-	$a_retval[] = '<input type="hidden" name="command" value="create_user">';
-	
-	$a_retval[] = '<div id="choose_username_form"><label>Choose a username:</label><br />';
-	$a_retval[] = '<input type="textarea" size="30" name="username" placeholder="Eg: The Dude" onblur="send_ajax_call_from_form(\'ajax.php\',\'choose_username_form\');" /><br />';
-	$a_retval[] = '<label class="errors">&nbsp;</label></div>';
-	
-	$a_retval[] = '<label>Choose a password:</label><br />';
-	$a_retval[] = '<input type="password" name="password" size="30" /><br /><br />';
-	$a_retval[] = '<label>Please enter an email (used for resetting your password):</label><br />';
-	$a_retval[] = '<input type="textarea" name="email" size="30" placeholder="Eg: thedudeabides@anywhere.net" onkeypress="if (event.keyCode==13){ $(\'#create_submit\').click(); }" /><br />';
-	$a_retval[] = '<label class="errors"></label><br />';
-	$a_retval[] = '<input id="create_submit" type="button" onclick="send_ajax_call_from_form(\'ajax.php\',\'create_user_form\');" value="Submit" /><br />';
-	$a_retval[] = '</form>';
-	return implode("\n", $a_retval);
+	ob_start();
+	?>
+	<form id="create_user_form">
+		<input type="hidden" name="command" value="create_user">
+
+		<div id="choose_username_form">
+			<label>Choose a username:</label><br />
+			<input type="textarea" size="30" name="username" placeholder="Eg: The Dude" onblur="send_ajax_call_from_form('ajax.php','choose_username_form');" /><br />
+			<label class="errors">&nbsp;</label>
+		</div>
+
+		<label>Choose a password:</label><br />
+		<input type="password" name="password" size="30" /><br /><br />
+		<label>Please enter an email (used for resetting your password):</label><br />
+		<input type="textarea" name="email" size="30" placeholder="Eg: thedudeabides@anywhere.net" onkeypress="if (event.keyCode==13){ $('#create_submit').click(); }" /><br />
+		<label class="errors"></label><br />
+		<input id="create_submit" type="button" onclick="send_ajax_call_from_form('ajax.php','create_user_form');" value="Submit" /><br />
+	</form>
+	<?php
+	$s_page = ob_get_contents();
+	ob_end_clean();
+
+	return $s_page;
 }
 
 function draw_create_page() {
