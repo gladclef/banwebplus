529c8330c3225f459e6d561274c1b68dd4e41649
Benjamin Bean <benjamin@poslavu.com>
2014-01-31 14:03:32 -0700

    updated scraping script to accept file output path arguments

---
+++ scraping/banweb.py
@@ -0,0 +1,263 @@
+import urllib2
+import copy
+import os
+import banweb_to_php
+from bs4 import BeautifulSoup
+import argparse
+
+def getRetardedSelectSyntaxFromBanweb(selectTag):
+	retval = []
+	for subject in selectTag.findAll("option"):
+		sid = subject["value"]
+		ssubject = str(subject)
+		sval = ssubject.split(">")[1].split("<")[0].strip()
+		retval.append([sid, sval])
+	return retval
+
+def writeSelectToFile(selectArray, filename, path, title):
+	f = open(path+filename, "w")
+	f.write(title+" = [['")
+	optionStrings = []
+	for optionParts in selectArray:
+		optionStrings.append("','".join(optionParts))
+	f.write("'],\n\t['".join(optionStrings))
+	f.write("']]")
+	f.close()
+
+class classList:
+	headers = []
+	classes = []
+	
+	def __init__(self):
+		self.headers = []
+		self.classes = []
+		
+	def getClasses(self):
+		return self.classes
+	def getHeaders(self):
+		return self.headers
+	
+	def isClass(self, bs4tr):
+		tds = bs4tr.findAll("td")
+		if (len(self.headers) == 0):
+			return False
+		if (len(tds) == len(self.headers)):
+			countWithContent = 0
+			for td in tds:
+				if (len(td.contents) == 0):
+					continue
+				text = td.contents[0]
+				if (text.strip() != ""):
+					countWithContent += 1
+			if (float(countWithContent) / float(len(self.headers)) > 0.5):
+				return True
+		return False
+	def addClass(self, bs4tr):
+		if (self.isClass(bs4tr)):
+			tds = bs4tr.findAll("td")
+			classStats = {}
+			for i in range(len(tds)):
+				td = tds[i]
+				if (len(td.contents) == 0):
+					continue
+				text = td.contents[0]
+				classStats[self.headers[i]] = text
+			self.classes.append(classStats)
+			print_verbose("adding class: "+str(classStats))
+			return True
+		return False
+	def setHeaders(self, bs4tr):
+		self.headers = []
+		ths = bs4tr.findAll("th")
+		for th in ths:
+			font = th.contents[0]
+			text = font.contents[0]
+			self.headers.append(text)
+		return True
+
+class term:
+	semester = "" #eg "201330"
+	classLists = {} # a dictionary of classlist objects, by subject
+	subjects = [] # an array of arrays[shortname, longname]
+	
+	def __init__(self, semester):
+		self.flush()
+		self.subjects = []
+		self.semester = semester
+		
+	def flush(self):
+		self.semester = ""
+		self.subjects = []
+		self.classLists = []
+	
+	def getSemester(self):
+		return self.semester
+
+	def semesterAsSeason(self):
+		semester = self.semester[4:6]+""
+		semesterName = "";
+		if (semester == "10"):
+			semesterName = "Summer"
+		elif (semester == "20"):
+			semesterName = "Fall"
+		elif (semester == "30"):
+			semesterName = "Spring"
+		return semesterName
+	
+	def getPrintedSemester(self):
+		semesterName = self.semesterAsSeason()
+		if (semesterName == ""):
+			semesterName = "Unknown"
+		return semesterName+" "+self.semester[:4]
+	
+	def _subjIndex(self, subject):
+		i = self.subjects.index(subject)
+		if (len(self.classLists) <= i):
+			self.addSubject(subject)
+		return i
+	def getClassList(self, subject):
+		return self.classLists[self._subjIndex(subject)]
+	def getClasses(self, subject):
+		return self.classLists[self._subjIndex(subject)].getClasses()
+	def getHeaders(self, subject):
+		return self.classLists[self._subjIndex(subject)].getHeaders()
+	def addSubject(self, subject):
+		if (not subject in self.subjects):
+			self.subjects.append(subject)
+			i = self.subjects.index(subject)
+			self.classLists.append(classList())
+	def getSubjects(self):
+		retval = []
+		for subject in self.subjects:
+			retval.append(subject)
+		return retval
+	# requires a beautiful soup table row
+	def addClass(self, subject, bs4tr):
+		return self.classLists[self._subjIndex(subject)].addClass(bs4tr)
+	# requires a beautiful soup table row
+	def setHeaders(self, subject, bs4tr):
+		return self.classLists[self._subjIndex(subject)].setHeaders(bs4tr)
+
+def getTerm(semester, subjects, parser):
+	t = term(semester[0])
+	for subject in subjects:
+		subjectName = subject[0]
+		if (type(parser.subject) == type("")):
+			if (parser.subject != subjectName):
+				continue;
+		url = "http://banweb7.nmt.edu/pls/PROD/hwzkcrof.P_UncgSrchCrsOff?p_term="+t.getSemester()+"&p_subj="+subjectName.replace(" ", "%20")
+		print url
+		page = urllib2.urlopen(url)
+		soup = BeautifulSoup(page)
+		trs = soup.findAll("tr")
+		trs = trs[1:] #discard the retarded row that banweb is retarded about
+		print_verbose("adding subject "+subjectName)
+		t.addSubject(subject)
+		if (len(trs) == 0):
+			print_verbose("no table rows...no classes subject")
+			continue
+		headersRow = trs[0]
+		print_verbose("adding headers "+str(headersRow))
+		t.setHeaders(subject, headersRow)
+		classesRows = trs[1:]
+		for tr in classesRows:
+			t.addClass(subject, tr)
+	return t
+
+def main(parser):
+	url = "http://banweb7.nmt.edu/pls/PROD/hwzkcrof.p_uncgslctcrsoff"
+
+	page = urllib2.urlopen(url)
+	
+	soup = BeautifulSoup(page)
+
+	path = ""
+	if (type(parser.path) == type("")):
+		path = parser.path
+
+	# load all terms and subjects from banweb
+	nodes = soup.findAll("select")
+	termsList = [] # each term -> [yyyyss, "season year"]
+	subjects = [] # each subject -> ["abriviation", "full name"]
+	for node in nodes:
+		nodeAttribs = dict(node.attrs)
+		if nodeAttribs[u"name"] == u"p_term":
+			termsList = getRetardedSelectSyntaxFromBanweb(node)[:]
+		elif nodeAttribs[u"name"] == u"p_subj":
+			subjects = getRetardedSelectSyntaxFromBanweb(node)[:]
+
+	# account for terms that have been listed before but
+	# have since been removed from banweb
+	import banweb_terms
+	for t in banweb_terms.terms:
+		if not t in termsList:
+			termsList.append(t)
+	termsList.sort()
+
+	# save the new listing of terms and subjects
+	writeSelectToFile(termsList, "banweb_terms.py", path, "terms")
+	writeSelectToFile(subjects, "banweb_subjects.py", path, "subjects")
+
+	# only process the terms that the user wants to
+	if (type(parser.semester) == type("")):
+		newtermsList = []
+		for t in termsList:
+			if (t[0] == parser.semester):
+				newtermsList.append(t)
+		termsList = newtermsList
+	print termsList
+
+	terms = []
+
+	latestYear = 0
+	latestSemester = 0
+	latestYearSemester = "201320"
+
+	for t in termsList:
+		year = int(t[0][0:4])
+		semester = int(t[0][4:6])
+		if (t[0] > latestYearSemester):
+			latestYear = year
+			latestSemester = semester
+			latestYearSemester = t[0]
+	
+	for t in termsList:
+		semester = t[0]
+		filename = "sem_"+semester+".py"
+		if (os.path.exists(filename) and semester != str(latestYear)+str(latestSemester)):
+			continue
+		terms.append(copy.copy(getTerm(t, subjects, parser)))
+
+	for t in terms:
+		name = t.getPrintedSemester()
+		subjects = t.getSubjects()
+		classes = []
+		for subj in subjects:
+			classes.append(t.getClasses(subj))
+		filename = "sem_"+t.getSemester()+".py"
+		print_verbose("writing to file "+path+filename)
+		try:
+			f = open(path+filename, "w")
+			f.write("name = \""+name+"\"\n")
+			f.write("subjects = "+subjects.__str__()+"\n")
+			f.write("classes = "+classes.__str__())
+			f.close()
+		except IOError as (errno,strerror):
+			print "I/O error({0}): {1}".format(errno, strerror)
+
+	banweb_to_php.main(parser)
+
+verbose = False
+def print_verbose(arg):
+	if verbose:
+		print arg
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Loads current class data from banweb.nmt.edu and saves it to local files for quick access (caching) and readable formats.")
+	parser.add_argument("--semester", type=str, help="choose the semester to load (eg '201430')")
+	parser.add_argument("--subject", type=str, help="choose the subject to load (eg 'CSE')")
+	parser.add_argument("--path", type=str, help="choose the location to save semester files to (must end in a slash, eg '/home/usr/stuff/')")
+	parser.add_argument("-v", action="store_true", dest="verbose", help="Print out verbose text about what is happening")
+	p = parser.parse_args()
+	verbose = p.verbose
+	main(p)
---
+++ scraping/banweb_to_php.py
@@ -0,0 +1,100 @@
+import re
+import banweb_terms
+import argparse
+
+# given a course from a sem_[0-9]+.py file, it will verify that the course is valid
+def courseIsValid(course):
+    for index in [u'Enroll', u'Seats', u'Hrs', u'CRN', u'Limit']:
+        if not re.search('\-?[0-9]+', course[index]):
+            #print index
+            return False
+    if not re.search('[\ U][\ M][\ T][\ W][\ R][\ F][\ S]', course[u'Days']):
+        if course[u'Days'] != ' ':
+            #print u'Days'
+            return False
+    if not re.search('[A-Z]+[\ ]+[0-9]+', course[u'Course']):
+        #print u'Course'
+        return False
+    if u'Time' in course:
+        if not re.search('[0-9]+\-[0-9]+', course[u'Time']):
+            #print u'Time'
+            return False
+    else:
+        course[u'Time'] = '';
+    return True
+
+def s(strToReplace):
+    strToReplace = strToReplace.replace("&amp;", '&')
+    strToReplace = strToReplace.replace("'", '')
+    return strToReplace
+
+# translates the given python file into something that php can understand
+def translate_file(filename, path):
+    m = __import__(filename)
+    fout = file(path+filename+".php", "w")
+    fout.write("<?php\n")
+    fout.write("$semesterData = array(\n")
+    
+    fout.write("\t'name'=>'"+s(m.name)+"',\n")
+    fout.write("\t'subjects'=>array(\n")
+    for subject in m.subjects:
+        fout.write("\t\t'"+s(subject[0])+"'=>'"+s(subject[1])+"',\n")
+    fout.write("\t),\n")
+    fout.write("\t'classes'=>array(\n")
+    
+    subject_index = 0
+    for subject in m.classes:
+        for course in subject:
+            if not courseIsValid(course):
+                print course
+                print ''
+                continue
+            fout.write("\t\tarray(\n");
+            fout.write("\t\t\t'subject'=>'"+s(m.subjects[subject_index][0])+"'")
+            for part in course:
+                fout.write(",\n\t\t\t'"+s(part)+"'=>'"+s(course[part])+"'")
+            fout.write("\n\t\t),\n")
+        subject_index += 1
+    
+    fout.write("\t)\n")
+    fout.write(");\n");
+    fout.write("$a_retval = array('name'=>$semesterData['name'], 'subjects'=>$semesterData['subjects'], 'classes'=>$semesterData['classes']);\n")
+    fout.write("$s_classes_json = json_encode($a_retval);\n")
+    fout.write("?>\n")
+    fout.close()
+
+def main(parser):
+    filenames = []
+    path = ""
+    
+    for term in banweb_terms.terms:
+        semester = term[0]
+        filenames.append("sem_"+semester)
+
+    if (type(parser.path) == type("")):
+        path = parser.path
+    
+    fout = file(path+"banweb_terms.php", "w")
+    fout.write("<?php\n")
+    fout.write("$terms = array(")
+    for term in banweb_terms.terms:
+        fout.write("\n\t array('")
+        first = True
+        for part in term:
+            if (first):
+                first = False
+            else:
+                fout.write('\',\'')
+            fout.write(part)
+        fout.write("'),")
+    fout.write("\n);")
+    fout.write("\n?>\n")
+    
+    for filename in filenames:
+        translate_file(filename, path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, help="choose the location to save semester files to (must end in a slash, eg '/home/usr/stuff/')")
+    p = parser.parse_args()
+    main(p)
