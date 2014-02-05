90731baa7571bf3999aa7154ac7675cebeb92aba
Benjamin Bean <benjamin@poslavu.com>
2014-02-05 15:46:25 -0700

    added feedback to set of exported data

--- resources/export_database_structure.php
+++ resources/export_database_structure.php
@@ -69,7 +69,8 @@ function getCommon_date() {
 	global $maindb;
 	return array(
 		array("name"=>"accesses", "index"=>"name", "rows"=>db_query("SELECT * FROM `{$maindb}`.`accesses`")),
-		array("name"=>"buglog", "index"=>"id", "rows"=>db_query("SELECT * FROM `{$maindb}`.`buglog`"))
+		array("name"=>"buglog", "index"=>"id", "rows"=>db_query("SELECT * FROM `{$maindb}`.`buglog`")),
+		array("name"=>"feedback", "index"=>"id", "rows"=>db_query("SELECT * FROM `{$maindb}`.`feedback`"))
 	);
 }
 
