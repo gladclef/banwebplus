83ac5f1e70995d240ac45f00b3ac59887320a7da
Benjamin Bean <benjamin@poslavu.com>
2014-08-19 16:01:58 -0600

    started on sharing schedules (in schedules tab)

--- tabs/Schedule.php
+++ tabs/Schedule.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once(dirname(__FILE__)."/../resources/globals.php");
+
 function init_schedule() {
-	return '
+	global $global_user;
+
+	$retval = '
     <table class=\'table_title\'><tr><td>
     <div class=\'centered\'>Add By CRN</div>
 </td></tr></table>
@@ -15,6 +19,16 @@ function init_schedule() {
 </td></tr></table>
 <div id=\'schedule_tab_user_recently_viewed_schedule\' class=\'centered\'>&nbsp;</div><br /><br />
 <input type=\'button\' style=\'display:none;\' name=\'onselect\' />';
+
+	if ($global_user->has_access("development")) {
+			$retval .= '
+<table class=\'table_title\'><tr><td>
+    <div class=\'centered\'>Share Schedule</div>
+</td></tr></table>
+<div id=\'schedule_tab_share_schedule\' class=\'centered\'>&nbsp;</div><br /><br />';
+	}
+
+	return $retval;
 }
 
 $tab_init_function = 'init_schedule';
