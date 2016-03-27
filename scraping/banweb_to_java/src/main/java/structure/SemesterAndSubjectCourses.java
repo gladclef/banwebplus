f4a4262b9753c2cd0646571237842c4cc2026730
password <password@beanweb>
2016-03-27 12:22:35 -0600

    moved source files into src/main/java/, added pom.xml to compile with maven

---
+++ scraping/banweb_to_java/src/main/java/structure/SemesterAndSubjectCourses.java
@@ -0,0 +1,53 @@
+package structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Contains all courses offered for a given subject + semester.
+ */
+public class SemesterAndSubjectCourses {
+	/** The semester of classes */
+	Semester semester = null;
+	/** The subject of classes */
+	Subject subject = null;
+	/** The classes matching the semester + subject */
+	List<Clazz> classes = new ArrayList<>();
+
+	public SemesterAndSubjectCourses(Semester semester, Subject subject) {
+		this.semester = semester;
+		this.subject = subject;
+	}
+
+	/**
+	 * @param clazz
+	 *            The {@link Clazz} to remeber for this subject + semester
+	 *            instance.
+	 */
+	public void addClass(Clazz clazz) {
+		classes.add(clazz);
+	}
+
+	/**
+	 * @return The classes registered in this instance.
+	 */
+	public List<Clazz> getClasses() {
+		return classes;
+	}
+	
+	/**
+	 * @return The semester registered for this instance.
+	 */
+	public Semester getSemester()
+	{
+		return semester;
+	}
+	
+	/**
+	 * @return The subject registered for this instance.
+	 */
+	public Subject getSubject()
+	{
+		return subject;
+	}
+}
